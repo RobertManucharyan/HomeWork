2d8136547952505133b5b3d3944b0c69e647941c
Rob <public@mail.ru>
2023-01-28 21:23:25 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+public class Main {
+    public static void main(String[] args) {
+        {
+            byte a = 23;
+            byte b = -12;
+            System.out.print(a);
+            System.out.print(b);
+
+            short c = 3000;
+            short d = -2565;
+            System.out.print(c);
+            System.out.print(d);
+
+            int e = 481115154;
+            int f = -11548625;
+            System.out.print(e);
+            System.out.print(f);
+
+            long g = -1643519365;
+            long h = 249515435;
+            System.out.print(g);
+            System.out.print(h);
+
+            System.out.print(Byte.MAX_VALUE);
+            System.out.print(Byte.MIN_VALUE);
+            System.out.print(Short.MAX_VALUE);
+            System.out.print(Short.MIN_VALUE);
+            System.out.print(Integer.MAX_VALUE);
+            System.out.print(Integer.MIN_VALUE);
+            System.out.print(Long.MAX_VALUE);
+            System.out.print(Long.MIN_VALUE);
+
+        }
+        {
+            float j = 2543.52f;
+            float k = 4854.841854f;
+            System.out.print(j);
+            System.out.print(k);
+
+            double l = 3446575857432435467d;
+            double m = 34567654345676543d;
+            System.out.print(l);
+            System.out.print(m);
+
+            System.out.print(Double.MAX_VALUE);
+            System.out.print(Double.MIN_VALUE);
+            System.out.print(Float.MAX_VALUE);
+            System.out.print(Float.MIN_VALUE);
+        }
+
+        {
+            char n = 0;
+            System.out.print(n);
+            char o = 65535;
+            System.out.print(o);
+
+            char s = 77;
+            char w = 109;
+            System.out.print(s);
+            System.out.print(w);
+
+            char t = 9;
+            System.out.print(t);
+        }
+        {
+            boolean p = true;
+            System.out.print(p);
+            boolean q = false;
+            System.out.print(q);
+        }
+        {
+            int year = 2011;
+            System.out.print(year);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
